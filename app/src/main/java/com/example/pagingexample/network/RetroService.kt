498133c2ba1052ded99ef84b0c48de0ccccbd0fb
emirk <emir.karabey1@hotmail.com>
2022-06-23 14:47:39 +0300

    Network layer added

---
+++ app/src/main/java/com/example/pagingexample/network/RetroService.kt
@@ -0,0 +1,10 @@
+package com.example.pagingexample.network
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetroService {
+
+    @GET("character")
+    suspend fun getDataFromAPI(@Query("page") query:Int):RickAndMortyList
+}
